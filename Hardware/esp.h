b9c6dbe0aaca3444a81782ad8115d097e4b990a8
xiaoming236 <85390590@qq.com>
2025-02-22 23:15:12 +0800

    Update esp.h

--- Hardware/esp.h
+++ Hardware/esp.h
@@ -2,16 +2,7 @@
 #ifndef __ESP_H
 #define __ESP_H
 
-// OneNET平台配置信息
-#define ONENET_DEVICE_ID    "SmFONzEyRDhZNnBRd3lzUkc4ZG5hUW1nejVOOGZlRjE="    // OneNET设备ID
-#define ONENET_API_KEY      "tcWzS6+3L48hbxc/BT3dmytPeVs9Q/fVMbkLzSezQTU="    // API密钥
-#define ONENET_PRODUCT_ID   "Ub99ifb8x7"     // 产品ID
-// OneNET平台配置信息
-#define ONENET_SERVER       "183.230.40.39"  // OneNET服务器地址
-#define ONENET_PORT        "6002"           // OneNET服务器端口
-// WiFi配置
-#define WIFI_SSID          "1234"           // WiFi名称
-#define WIFI_PASSWORD      "123456789"      // WiFi密码
+
 
 // 函数声明
 void ESP8266_Init(void);
